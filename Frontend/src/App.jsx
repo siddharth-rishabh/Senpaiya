288fa4e82eb093dc2d2617c6aafcb21b068781aa
Siddharth <siddharthrishabh019@gmail.com>
2025-10-22 14:40:52 +0530

    fixed case sensitive error

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Login from "../pages/Login";
-import Home from "../pages/home";
+import Home from "../pages/Home";
 import Signup from "../pages/Signup";
 import Drops from "../pages/Drops";
 import Ping from "../pages/Ping";
